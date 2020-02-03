4af10d88a9145274b6a8793f40924eff763047f4
Kai Li 李凯 <tsinghua.kaili@gmail.com>
2020-02-03 11:50:41 +0800

    Update Conv_TasNet.py

--- Conv_TasNet.py
+++ Conv_TasNet.py
@@ -164,7 +164,6 @@ class ConvTasNet(nn.Module):
        N	Number of ﬁlters in autoencoder
        L	Length of the ﬁlters (in samples)
        B	Number of channels in bottleneck and the residual paths’ 1 × 1-conv blocks
-       Sc	Number of channels in skip-connection paths’ 1 × 1-conv blocks
        H	Number of channels in convolutional blocks
        P	Kernel size in convolutional blocks
        X	Number of convolutional blocks in each repeat
